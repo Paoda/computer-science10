cd458cb68bdf53ca93013ac81be6ff0f046a8498
Paoda <musukarekai@gmail.com>
2017-04-20 10:31:13 -0500

    almost completed calcTaxes function

--- payrollCalculations/source.cpp
+++ payrollCalculations/source.cpp
@@ -20,7 +20,8 @@ Union Dues: 0.4%
 
 */
 
-float grossIncome(float hoursWorkedF) float calcTaxes();
+float grossIncome(float hoursWorkedF);
+float calcTaxes(float grossPayF);
 float displayReceipt();
 
 #include <iostream>
@@ -30,11 +31,12 @@ using namespace std;
 
 int main()
 {
-    float hoursWorked grossPay;
+    float hoursWorked grossPay, netPay;
     cout << "Please enter in the amount of Hours you have worked.";
     cin >> hoursWorked;
 
     grossPay = grossIncome(hoursWorked);
+    netpay = calcTaxes(grossPay);
 
     return 0;
 }
@@ -59,21 +61,23 @@ float grossIncome(float hoursWokedF)
     else if (hoursWorked > 45)
     {
         hoursWorkedF = hoursWorkedF - 45;
-        grossPay = (40 * 10.5) + (5 * (10.5 * 1.5)) + (hoursWorkedF * (10.5 * (10.5 * 2)));
+        grossPayF = (40 * 10.5) + (5 * (10.5 * 1.5)) + (hoursWorkedF * (10.5 * (10.5 * 2)));
     }
 
     return grossPayF
 } //end grossIncome
 
-float calcTaxes(float grossPayF)
-{
-    /*
-    Tax: 31%
-    CPP: 2.4%
-    UIP: 1.9%
-    Union Dues: 0.4%
-    */
+float calcTaxes(float grossPayF) {
+    float taxedAmountF, netPayF;
 
+    taxedAmountF = grossPayF * 0.31; //Tax
+    taxedAmountF += grossPayF * 0.024; //CPP
+    taxedAmountF += grossPayF * 0.019; //UIC
+    taxedAmountF += grossPayF * 0.004; //Union Dues
+
+    netPayF = grossPayF - taxedAmountF;
+
+    return netPayF;
 } //end calcTaxes
 
 float displayReceipt()
